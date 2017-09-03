35a3546d7e704375ec4d4211cc7604b7e6f0557a
hrm123 <bestcoder0@yahoo.com>
2017-09-03 09:28:38 -0400

    typescript delta

--- offlineTodos1/index.android.js
+++ offlineTodos1/index.android.js
@@ -17,7 +17,7 @@ export default class offlineTodos1 extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native 1!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
