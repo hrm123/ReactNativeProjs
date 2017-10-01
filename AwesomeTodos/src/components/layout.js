ea78edf31920069fd469b361f84fd0aa4a207438
hrm123 <bestcoder0@yahoo.com>
2017-10-01 15:20:00 -0400

    css stuff

--- AwesomeTodos/src/components/layout.js
+++ AwesomeTodos/src/components/layout.js
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import {Container, Header, Content, Footer, Title} from 'native-base';
+import {Container, StyleProvider, Header, Content, Footer, Input, Item, FooterTab, Badge, Title, Left, Right, Body, Icon, Button} from 'native-base';
+import getTheme from '../../native-base-theme/components';
+import material from '../../native-base-theme/variables/material';
+
 export default class LayoutExample extends Component {
     render() {
         return (
-        <Container>
-            <Header>
-                <Title>Header</Title>
-            </Header>
+            <StyleProvider style={getTheme(material)}>
+                <Container>
+                    <Header searchBar rounded>
+                            <Item>
+                                <Icon name="ios-search" />
+                                <Input placeholder="Filter Todos" />
+                            </Item>
 
-            <Content>
-                <Text> Your main content goes here </Text>
-            </Content>
+                    </Header>
 
-            <Footer>
-                <Title>Footer</Title>
-            </Footer>
-        </Container>
+                    <Content>
+                        <Text> Your main content goes here </Text>
+                    </Content>
+
+                    <Footer>
+                        <FooterTab>
+                            <Button badge vertical>
+                                <Badge><Text style={{color: 'white'}}>2</Text></Badge>
+                                <Icon name="ios-add" />
+                                <Text>Pending</Text>
+                            </Button>
+                            <Button vertical>
+                                <Icon name="md-checkmark" />
+                                <Text>Done</Text>
+                            </Button>
+                            <Button vertical>
+                                <Icon name="trash" />
+                                <Text>Deleted</Text>
+                            </Button>
+                            <Button vertical>
+                                <Icon name="person" />
+                                <Text>Settings</Text>
+                            </Button>
+                        </FooterTab>
+
+                    </Footer>
+                </Container>
+            </StyleProvider>
     );
     }
 }
