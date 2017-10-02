e30039f9f83490fd7544e984c536d318f605005b
hrm123 <bestcoder0@yahoo.com>
2017-10-02 04:06:27 -0400

    ui elements test

--- AwesomeTodos/src/components/layout.js
+++ AwesomeTodos/src/components/layout.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { Text , TextInput} from 'react-native';
-import {Container, StyleProvider, Header, Content, Footer, Input, Item, FooterTab, Badge, Title, Left, Right, Body, Icon, Button} from 'native-base';
+import {Container, StyleProvider, Header, Content, Footer, Input, Item, FooterTab, Badge, Tab, Tabs, TabHeading, Title, Left, Right, Body, Icon, Button} from 'native-base';
 import getTheme from '../../native-base-theme/components';
 import material from '../../native-base-theme/variables/material';
+
+
+const Tab1 = () => <Text>tab1</Text>
+const Tab2 = () => <Text>tab2</Text>
+const Tab3 = () => <Text>tab3</Text>
+const Tab4 = () => <Text>tab4</Text>
 
 export default class LayoutExample extends Component {
     render() {
@@ -11,14 +17,14 @@ export default class LayoutExample extends Component {
         return (
             <StyleProvider style={styl}>
                 <Container>
-                    <Header searchBar rounded>
+                    <Header searchBar rounded hasTabs >
                         <Left>
                             <Icon name="ios-menu" />
                         </Left>
-                        <Body >
+                        <Body  >
                             <Item >
                                 <Icon name="ios-search" />
-                                <Input placeholder="Type Filter text" placeholderTextColor="#fff"/>
+                                <Input placeholder="Type Filter text"  style={{backgroundColor:"transparent"}}/>
                             </Item>
                         </Body>
                         <Right>
@@ -26,9 +32,25 @@ export default class LayoutExample extends Component {
                         </Right>
                     </Header>
 
-                    <Content>
-                        <Text> Your main content goes here </Text>
-                    </Content>
+                    <Tabs>
+                        <Tab heading={ <TabHeading>
+                            <Button badge>
+                                <Icon name="ios-add" />
+                                <Badge><Text style={{color: 'white'}}>2</Text></Badge>
+                            </Button>
+                            </TabHeading>}>
+                            <Tab1 />
+                        </Tab>
+                        <Tab heading={ <TabHeading><Icon name="md-checkmark" /></TabHeading>}>
+                            <Tab2 />
+                        </Tab>
+                        <Tab heading={ <TabHeading><Icon name="trash" /></TabHeading>}>
+                            <Tab3 />
+                        </Tab>
+                        <Tab heading={ <TabHeading><Icon name="person" /></TabHeading>}>
+                            <Tab4 />
+                        </Tab>
+                    </Tabs>
 
                     <Footer>
                         <FooterTab>
